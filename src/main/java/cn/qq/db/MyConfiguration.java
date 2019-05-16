82c25c69f37c79a4671cdd0d6f34d1b2c3b36e8b
Quantriple <499244680@qq.com>
2019-05-16 15:54:08 +0800

    登录相关

--- src/main/java/cn/qq/db/MyConfiguration.java
+++ src/main/java/cn/qq/db/MyConfiguration.java
@@ -1,10 +1,13 @@
 package cn.qq.db;
 
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import javax.sql.DataSource;
@@ -13,6 +16,7 @@ import javax.sql.DataSource;
  * Created by Administrator on 2019/5/8.
  */
 @Configuration
+@MapperScan(basePackages = {"cn.qq.dao"})
 public class MyConfiguration {
 
     @Autowired
@@ -20,9 +24,11 @@ public class MyConfiguration {
     private DataSource dataSourc1e;
 
     @Bean(name = "SqlSessionFactory")
-    public SqlSessionFactoryBean sqlSessionFactory() throws Exception {
+    public SqlSessionFactoryBean sqlSessionFactory(@Value("${mybatis.mapper-locations}") String mapperLocations,@Value("${mybatis.type-aliases-package}")String typeAliasesPackage) throws Exception {
         SqlSessionFactoryBean sessionFactoryBean = new SqlSessionFactoryBean();
         sessionFactoryBean.setDataSource(dataSourc1e);
+        sessionFactoryBean.setMapperLocations((new PathMatchingResourcePatternResolver().getResources(mapperLocations)));
+        sessionFactoryBean.setTypeAliasesPackage(typeAliasesPackage);
         return sessionFactoryBean;
     }
     @Bean(name = "TransactionManager")
