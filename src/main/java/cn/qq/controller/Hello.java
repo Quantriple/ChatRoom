5ca69c7a96fb2d4f77993cc0a3b6e48dfb53dc29
Quantriple <499244680@qq.com>
2019-05-23 14:50:47 +0800

    序列化和工具类上传

--- src/main/java/cn/qq/controller/Hello.java
+++ src/main/java/cn/qq/controller/Hello.java
@@ -1,15 +1,8 @@
 package cn.qq.controller;
 
-import cn.qq.dao.UsersMapper;
-import cn.qq.model.Users;
-import cn.qq.utils.Local2Date;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.Resource;
-import java.util.Date;
 
 /**
  * Created by Administrator on 2019/5/15.
@@ -18,21 +11,14 @@ import java.util.Date;
 @Slf4j
 public class Hello {
 
-    @Autowired
-    private UsersMapper usersMapper;
+
 
     @RequestMapping("/test")
     public String hello(){
-        Users users=new Users();
-        users.setUserName("qqq");
-        users.setPassword("123");
-        users.setCreatetime(Local2Date.Local2Date());
-        //LocalDate.now()
-        try{
-        usersMapper.insert(users);}
-        catch (Exception e){
-            e.getMessage();
-        }
-        return  "hello";
+        log.debug("debug");
+        log.info("info");
+        log.error("error");
+        log.warn("warn");
+        return  null;
     }
 }
